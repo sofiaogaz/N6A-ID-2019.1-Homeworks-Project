69890ccb724053adc2b2ba90db837ad27346fbc0
Santiago Mendez <santi17mendez@hotmail.com>
2019-03-21 01:21:32 -0300

    Base Code

---
+++ Codigo/WebApi/Homeworks.BusinessLogic.Interface/IHomeworkLogic.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using Homeworks.Domain;
+
+namespace Homeworks.BusinessLogic.Interface
+{
+    public interface IHomeworkLogic : IDisposable
+    {
+        Homework Create(Homework homework);
+
+        void Remove(Guid id);
+
+        Homework Update(Guid id, Homework homework);
+
+        Exercise AddExercise(Guid id, Exercise exercise);
+
+        Homework Get(Guid id);
+
+        IEnumerable<Homework> GetAll();
+    }
+}
---
+++ Codigo/WebApi/Homeworks.DataAccess/BaseRepository.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using Homeworks.DataAccess.Interface;
+using Microsoft.EntityFrameworkCore;
+
+namespace Homeworks.DataAccess
+{
+    public abstract class BaseRepository<T> : IRepository<T> where T : class
+    {
+        protected DbContext Context {get; set;}
+
+        public void Add(T entity) 
+        {
+            Context.Set<T>().Add(entity);
+        }
+
+        public void Remove(T entity) 
+        {
+            Context.Set<T>().Remove(entity);
+        }
+
+        public void Update(T entity) 
+        {
+            Context.Entry(entity).State = EntityState.Modified;
+        }
+
+        public abstract IEnumerable<T> GetAll();
+
+        public abstract T Get(Guid id);
+
+        public void Save() 
+        {
+            Context.SaveChanges();
+        }
+
+        private bool disposedValue = false;
+
+        protected virtual void Dispose(bool disposing)
+        {
+            if (!disposedValue)
+            {
+                if (disposing)
+                {
+                    Context.Dispose();
+                }
+                disposedValue = true;
+            }
+        }
+
+        public void Dispose()
+        {
+            Dispose(true);
+            GC.SuppressFinalize(this);
+        }
+    }
+}
