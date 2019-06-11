9c092dafbdd95ac2eb74ea6021f4bb45029a5da6
Santiago Mendez <santi17mendez@hotmail.com>
2019-06-11 15:43:00 -0300

    Fix route

--- Codigo/Angular/src/app/guards/homework-detail.guard.ts
+++ Codigo/Angular/src/app/guards/homework-detail.guard.ts
@@ -7,8 +7,8 @@ export class HomeworkDetailGuard implements CanActivate {
   constructor(private _router: Router) { }
 
   canActivate(route: ActivatedRouteSnapshot): boolean {
-    let id = +route.url[1].path;
-    if (isNaN(id) || id < 1) {
+    let id = route.url[1].path;
+    if (!isNaN(+id)) {
         alert('La id de la tarea no es valida');
         // redirigimos (a traves de una navegacion), a /homeworks
         this._router.navigate(['/homeworks']);
--- Codigo/Angular/src/app/homework-detail/homework-detail.component.ts
+++ Codigo/Angular/src/app/homework-detail/homework-detail.component.ts
@@ -16,7 +16,7 @@ export class HomeworkDetailComponent implements OnInit {
   ngOnInit() : void {
     // let (es parte de ES2015) y define una variable que vive en este scope
     // usamos el nombre del parámetro que uamos en la configuración de la ruta y lo obtenemos
-    let id =+ this._currentRoute.snapshot.params['id'];
+    let id = this._currentRoute.snapshot.params['id'];
     // definimos el string con interpolacion 
     this.pageTitle +=  `: ${id}`;
   }
